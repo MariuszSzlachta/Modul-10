44e072a1d9ae2a42d10a1d1deaf6a05f17562f1b
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-06-02 00:16:20 +0200

    Fix problems with carousel

--- 10.2/js/script.js
+++ 10.2/js/script.js
@@ -1,21 +1,19 @@
 var templateSlide = document.getElementById('template-carousel').innerHTML;
-
-console.log('temp: ' + templateSlide);
+var carouselCells = document.querySelectorAll('.container');
 
 Mustache.parse(templateSlide);
-var items = '';
+
+var items = [];
 
 for (var i = 0; i < data.length; i++) {
-  items += Mustache.render(templateSlide, data[i]);
-  console.log(templateSlide);
-  console.log(data[i]);
+  items.push(Mustache.render(templateSlide, data[i]));
+}
+for (i =0; i < carouselCells.length; i++){
+  carouselCells[i].innerHTML = items[i];
 }
 
-var listItems = Mustache.render(templateSlide, {description: items})
-var container = document.querySelector('.container');
-container.innerHTML = listItems;
 
-
+// flickity
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity(elem, {
   // options
@@ -24,19 +22,3 @@ var flkty = new Flickity(elem, {
   hash: true,
   pageDots: false
 });
-
-// element argument can be a selector string
-
-// button restart
-// var btnRestart = document.querySelector('.btn-restart');
-// btnRestart.addEventListener('click', function(){
-//   flkty.select(0);
-// });
-
-// scroll
-// var progressBar = document.querySelector('.progress-bar');
-
-// flkty.on( 'scroll', function( progress ) {
-//   progress = Math.max( 0, Math.min( 1, progress ) );
-//   progressBar.style.width = progress * 100 + '%';
-// });
